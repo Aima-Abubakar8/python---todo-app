3376d4a1138c2a17d97c4a5ed25b6e5b456b6114
Aima-Abubakar8 <aimaabubakar8@gmail.com>
2026-08-11 03:44:42 -0700

    Remove student class and instance creation

--- python.practice1.py/oops.practice.py
+++ python.practice1.py/oops.practice.py
@@ -1,16 +1,1 @@
-class student :
-    def __init__(self,name,marks):
-        self.name = name 
-        self.marks = marks
-    print("adding new students in database")
 
-
-S1 = student("aima" , 99)
-print(S1.name , S1.marks)
-
-
-S2 = student("ali" , 98)
-print(S2.name ,  S2.marks)
-
-
- 
