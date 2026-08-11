7478297852d2a3433e3295094d172297d12fc40f
Aima-Abubakar8 <aimaabubakar8@gmail.com>
2026-08-11 03:44:18 -0700

    Remove unused calculator code
    Remove commented-out code for a simple calculator.

--- python.practice1.py/calculator2.py
+++ python.practice1.py/calculator2.py
@@ -1,12 +1,1 @@
 
-
-
-# print ("=== super easy calculator ===")
-
-# num1 = int(input("enter a number :"))
-# num2 = int(input("enter a number :"))
-
-# print("add:" , num1 + num2)
-# print("substract:" , num1 - num2)
-# print("division:" , num1 / num2)
-# print("multplication:" , num1 * num2)
